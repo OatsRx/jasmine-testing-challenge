1934b452b27cceef29898a6725a025cffec15643
EC2 Default User <ec2-user@ip-172-31-41-154.ec2.internal>
2019-12-16 13:44:42 +0000

    fixed spelling mistakes between files which was causeing tests to fail, mainly becasue of capitalisation

--- scripts/drink.js
+++ scripts/drink.js
@@ -1,20 +1,20 @@
 whatCanIDrink = function(age) {
-    if (age < 0) {
-        return "Sorry. I can't tell you what to drink as yourthat age is incorrect";
-    }
-    else if (age < 14) {
-        return "Drink juice";
-    }
-    else if (age < 18 && age >=14) {
-        return "Drink fanta";
-    }
-    else if (age < 21 && age >=18) {
-        return "Drink beer";
-    }
-    else if (age < 130) {
-        return "Drink whiskey";
-    }
-    else {
-        return "Sorry. I can't tell you what to drink as yourthat age is incorrect";
-    }
-}
+	if (age < 0) {
+		return "Sorry. I can’t tell what drink because that age is incorrect!";
+	}
+	else if (age < 14) {
+		return "Drink Toddy";
+	}
+	else if (age < 18 && age >=14) {
+		return "Drink Coke";
+	}
+	else if (age < 21 && age >=18) {
+		return "Drink Beer";
+	}
+	else if (age < 130) {
+		return "Drink Whisky";
+	} 
+	else {
+		return "Sorry. I can’t tell what drink because that age is incorrect!"
+	}
+};
